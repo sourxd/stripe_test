65debde249e0fc97160a4cc6d86542a74d843ac5
Oleg Barovskiy <oleg.sour@gmail.com>
2023-12-08 09:41:48 +0300

    readme

--- stripe_test/settings.py
+++ stripe_test/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.226', '84.52.88.235']
 
